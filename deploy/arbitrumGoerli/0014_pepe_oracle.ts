5b021f040140a63f2badcb1559f931e8664f5112
voith <voithjm1@gmail.com>
2023-08-07 18:50:45 +0530

    added scripts and tests for deploying pepe on goerli

---
+++ deploy/arbitrumGoerli/0014_pepe_oracle.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Deployment } from "hardhat-deploy/dist/types";
+import { hardhatArguments } from "hardhat";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  if (hardhatArguments.network !== "arbitrumGoerli") {
+    return;
+  }
+
+  if (!process.env.COORDINATOR_ID) {
+    console.log("please set COORDINATOR_ID in env");
+    return;
+  }
+
+  const { deployments, getNamedAccounts, ethers } = hre;
+  const { deploy, get, getOrNull, getNetworkName } = deployments;
+  const { deployer } = await getNamedAccounts();
+  // Note: This will be the Oracle address on mainnet. No need to deploy.
+  const pepeOracleDeployment = await deploy("ChainlinkPEPEAgregatorV3", {
+    from: deployer,
+    skipIfAlreadyDeployed: true,
+    log: true,
+    autoMine: true,
+  });
+
+  await deploy("PEPEOracle", {
+    contract: "ChainlinkFeedOracle",
+    args: [pepeOracleDeployment.address],
+    from: deployer,
+    skipIfAlreadyDeployed: true,
+    log: true,
+  });
+};
+
+export default func;
+func.tags = ["PEPE"];
