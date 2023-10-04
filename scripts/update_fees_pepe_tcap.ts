1888bd404ee2744fcd5da04136a284525444ac46
voith <voithjm1@gmail.com>
2023-10-04 22:47:37 +0530

    clean up

--- scripts/update_fees_pepe_tcap.ts
+++ scripts/update_fees_pepe_tcap.ts
@@ -29,13 +29,17 @@ async function main() {
   console.log("Old takerFee for TCAP long product is", await tcapLong.takerFee());
   console.log("Old takerFee for TCAP Short product is", await tcapShort.takerFee());
   let tx = await tcapLong.updateMakerFee(newMakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await tcapShort.updateMakerFee(newMakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await tcapLong.updateTakerFee(newTakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await tcapShort.updateTakerFee(newTakerfee);
-  await tx.wait();
+  await tx.wait()
+  tx = await tcapLong.settle()
+  await tx.wait()
+  tx = await tcapShort.settle()
+  await tx.wait()
   console.log("New makerFee for TCAP long product is", await tcapLong.makerFee());
   console.log("New makerFee for TCAP Short product is", await tcapShort.makerFee());
   console.log("New takerFee for TCAP long product is", await tcapLong.takerFee());
@@ -43,25 +47,28 @@ async function main() {
 
   console.log("Old positionFee for PERPE long product is", await pepeLong.positionFee());
   console.log("Old positionFee for PERPE Short product is", await pepeShort.positionFee());
-  tx = await pepeLong.updatePositionFee(newPositionFee)
-  await tx.wait();
-  tx = await pepeShort.updatePositionFee(newPositionFee)
-  await tx.wait();
-  console.log("New positionFee for PERPE long product is", await pepeLong.positionFee());
-  console.log("New positionFee for PERPE Short product is", await pepeShort.positionFee());
-
   console.log("Old makerFee for PERPE long product is", await pepeLong.makerFee());
   console.log("Old makerFee for PERPE Short product is", await pepeShort.makerFee());
   console.log("Old takerFee for PERPE long product is", await pepeLong.takerFee());
   console.log("Old takerFee for PERPE Short product is", await pepeShort.takerFee());
+  tx = await pepeLong.updatePositionFee(newPositionFee)
+  await tx.wait()
+  tx = await pepeShort.updatePositionFee(newPositionFee)
+  await tx.wait()
   tx = await pepeLong.updateMakerFee(newMakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await pepeShort.updateMakerFee(newMakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await pepeLong.updateTakerFee(newTakerfee);
-  await tx.wait();
+  await tx.wait()
   tx = await pepeShort.updateTakerFee(newTakerfee);
-  await tx.wait();
+  await tx.wait()
+  tx = await pepeLong.settle()
+  await tx.wait()
+  tx = await pepeLong.settle()
+  await tx.wait()
+  console.log("New positionFee for PERPE long product is", await pepeLong.positionFee());
+  console.log("New positionFee for PERPE Short product is", await pepeShort.positionFee());
   console.log("New makerFee for PERPE long product is", await pepeLong.makerFee());
   console.log("New makerFee for PERPE Short product is", await pepeShort.makerFee());
   console.log("New takerFee for PERPE long product is", await pepeLong.takerFee());
